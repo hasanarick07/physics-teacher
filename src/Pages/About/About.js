fb9c6a2ab2f6504b5b50ca444025efce142aae3d
Arick Hasan <hasanarick000@gmail.com>
2022-04-18 23:37:39 +0600

    final

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -5,88 +5,90 @@ const About = () => {
   return (
     <div className=" ">
       <img className="w-48 mx-auto rounded-xl mb-3" src={arick} alt="" />
-      <table className="text-left mx-auto text-white table-auto border-separate border border-slate-500">
+      <table className="text-left mx-auto text-white table-auto border-separate border border-slate-100">
         <thead>
           <tr>
-            <th className="border text-xl font-medium border-slate-600">
+            <th className="border text-xl font-medium border-slate-200 p-4">
               Title
             </th>
-            <th className="border border-slate-600">Description</th>
+            <th className="border border-slate-200 p-4">Description</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td className="border text-xl font-medium border-slate-600">
+            <td className="border text-xl font-medium border-slate-200 p-4">
               Name :
             </td>
-            <td className="border border-slate-600">Md. Arick Hasan Rana</td>
+            <td className="border border-slate-200 p-4">
+              Md. Arick Hasan Rana
+            </td>
           </tr>
           <tr>
-            <td className="border text-xl font-medium border-slate-600">
+            <td className="border text-xl font-medium border-slate-200 p-4">
               Father's Name :
             </td>
-            <td className="border border-slate-600">Md. Esrafil Hossen </td>
+            <td className="border border-slate-200 p-4">Md. Esrafil Hossen </td>
           </tr>
           <tr>
-            <td className="border text-xl font-medium border-slate-600">
+            <td className="border text-xl font-medium border-slate-200 p-4">
               Mother's Name :
             </td>
-            <td className="border border-slate-600">Nasima Akhter </td>
+            <td className="border border-slate-200 p-4">Nasima Akhter </td>
           </tr>
           <tr>
-            <td className="border text-xl font-medium border-slate-600">
+            <td className="border text-xl font-medium border-slate-200 p-4">
               Date of Birth :
             </td>
-            <td className="border border-slate-600">February 7, 1995</td>
+            <td className="border border-slate-200 p-4">February 7, 1995</td>
           </tr>
           <tr>
-            <td className="border text-xl font-medium border-slate-600">
+            <td className="border text-xl font-medium border-slate-200 p-4">
               Gender :
             </td>
-            <td className="border border-slate-600">Male </td>
+            <td className="border border-slate-200 p-4">Male </td>
           </tr>
           <tr>
-            <td className="border text-xl font-medium border-slate-600">
+            <td className="border text-xl font-medium border-slate-200 p-4">
               Marital Status :
             </td>
-            <td className="border border-slate-600">Single </td>
+            <td className="border border-slate-200 p-4">Single </td>
           </tr>
           <tr>
-            <td className="border text-xl font-medium border-slate-600">
+            <td className="border text-xl font-medium border-slate-200 p-4">
               Nationality :
             </td>
-            <td className="border border-slate-600">Bangladeshi</td>
+            <td className="border border-slate-200 p-4">Bangladeshi</td>
           </tr>
           <tr>
-            <td className="border  text-xl font-medium border-slate-600">
+            <td className="border  text-xl font-medium border-slate-200 p-4">
               National Id No :
             </td>
-            <td className="border border-slate-600">2369649237</td>
+            <td className="border border-slate-200 p-4">2369649237</td>
           </tr>
           <tr>
-            <td className="border text-xl font-medium border-slate-600">
+            <td className="border text-xl font-medium border-slate-200 p-4">
               Religion :
             </td>
-            <td className="border border-slate-600">Islam</td>
+            <td className="border border-slate-200 p-4">Islam</td>
           </tr>
           <tr>
-            <td className="border text-xl font-medium border-slate-600">
+            <td className="border text-xl font-medium border-slate-200 p-4">
               Passport No :
             </td>
-            <td className="border border-slate-600">EA0257567</td>
+            <td className="border border-slate-200 p-4">EA0257567</td>
           </tr>
           <tr>
-            <td className="border text-xl font-medium border-slate-600">
+            <td className="border text-xl font-medium border-slate-200 p-4">
               Passport Issue Date :
             </td>
-            <td className="border border-slate-600">4/2/2019</td>
+            <td className="border border-slate-200 p-4">4/2/2019</td>
           </tr>
           <tr>
-            <td className="border text-xl font-medium border-slate-600">
+            <td className="border text-xl font-medium border-slate-200 p-4">
               {" "}
               Permanent Address :
             </td>
-            <td className="border border-slate-600">
+            <td className="border border-slate-200 p-4">
               Md. Arick Hasan, Holding no.301, Uttar Gazirchat,
               <br /> Alia Madrasah, EPZ, Savar, Dhaka 1349
             </td>
--- src/Pages/Shared/ContinueWithGGF/ContinueWithGGF.js
+++ src/Pages/Shared/ContinueWithGGF/ContinueWithGGF.js
@@ -1,14 +1,23 @@
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
 import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useLocation, useNavigate } from "react-router-dom";
 import auth from "../../../firebase.init";
 import googleIcon from "../../../images/Google-icon.png";
+import githubIcon from "../../../images/pngegg.png";
 
 const ContinueWithGGF = () => {
   const googleProvider = new GoogleAuthProvider();
   const googleSignIn = () => {
     signInWithPopup(auth, googleProvider);
+  };
+  const githubProvider = new GithubAuthProvider();
+  const githubSignIn = () => {
+    signInWithPopup(auth, githubProvider);
   };
   const navigate = useNavigate();
   const [user] = useAuthState(auth);
@@ -26,6 +35,15 @@ const ContinueWithGGF = () => {
         >
           <img className="w-6 mx-3 " src={googleIcon} alt="" />
           Continue with Google
+        </button>
+      </div>
+      <div className="mt-10" onClick={githubSignIn}>
+        <button
+          className="text-white flex bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-600 font-medium rounded-lg text-lg 
+              w-3/4  sm:w-auto md:px-20 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-700 mx-auto"
+        >
+          <img className="w-8 mx-3 " src={githubIcon} alt="" />
+          Continue with GitHub
         </button>
       </div>
     </div>
